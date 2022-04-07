6e81ec516d006f122d85e5c660602601a5664a67
Odin Siljebøl <odin.berg@hotmail.co.uk>
2022-04-07 09:20:20 +0200

    Initial Commit

---
+++ shortcutCodingChallenge/shortcutCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  shortcutCodingChallengeApp.swift
+//  shortcutCodingChallenge
+//
+//  Created by Odin Siljebøl on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct shortcutCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
